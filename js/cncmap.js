ef88d292c2fbf581633e4487a6336c636a26a6c4
Jesse Lamas Calvillo <jesse.calvillo@gmail.com>
2018-09-28 23:56:52 -0700

    world map

--- js/cncmap.js
+++ js/cncmap.js
@@ -6,10 +6,10 @@ var width = 960,
     height = 1160;
 
 var projection = d3.geo.albers()
-    .center([0, 55.4])
+    // .center([0, 55.4])
     .rotate([4.4, 0])
     .parallels([50, 60])
-    .scale(1200 * 5)
+    .scale(1200 * 0.25)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -40,26 +40,26 @@ d3.json("data/uk.json", function(error, uk) {
       .attr("d", path)
       .attr("class", "subunit-boundary IRL");
 
-  svg.selectAll(".subunit-label")
-      .data(subunits.features)
-    .enter().append("text")
-      .attr("class", function(d) { return "subunit-label " + d.id; })
-      .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.properties.name; });
-
-  svg.append("path")
-      .datum(places)
-      .attr("d", path)
-      .attr("class", "place");
-
-  svg.selectAll(".place-label")
-      .data(places.features)
-    .enter().append("text")
-      .attr("class", "place-label")
-      .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-      .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
-      .attr("dy", ".35em")
-      .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
-      .text(function(d) { return d.properties.name; });
+  // svg.selectAll(".subunit-label")
+  //     .data(subunits.features)
+  //   .enter().append("text")
+  //     .attr("class", function(d) { return "subunit-label " + d.id; })
+  //     .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+  //     .attr("dy", ".35em")
+  //     .text(function(d) { return d.properties.name; });
+  //
+  // svg.append("path")
+  //     .datum(places)
+  //     .attr("d", path)
+  //     .attr("class", "place");
+  //
+  // svg.selectAll(".place-label")
+  //     .data(places.features)
+  //   .enter().append("text")
+  //     .attr("class", "place-label")
+  //     .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+  //     .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+  //     .attr("dy", ".35em")
+  //     .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
+  //     .text(function(d) { return d.properties.name; });
 });
